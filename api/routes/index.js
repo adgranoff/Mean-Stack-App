ffac5a6e848d843eb048228f1486004ee5165a5b
“adgranoff” <adamgranoff@hotmail.com>
2018-02-11 22:17:29 -0500

    Setup Mongoose GET APIs

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var ctrlHotels = require('../controllers/hotels.controllers.js');
+var ctrlReviews = require('../controllers/reviews.controllers.js');
+
+// hotel routes
 
 router
     .route('/hotels')
@@ -14,5 +17,15 @@ router
 router
     .route('/hotels/new')
     .post(ctrlHotels.hotelsAddOne);
+
+//review routes
+
+router
+    .route('/hotels/:hotelId/reviews')
+    .get(ctrlReviews.reviewsGetAll);
+
+router
+   .route('/hotels/:hotelId/reviews/:reviewId')
+    .get(ctrlReviews.reviewsGetOne);
 
 module.exports = router;
