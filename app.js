174666371e59a26f939b479b96416065842f3a05
“adgranoff” <adamgranoff@hotmail.com>
2018-02-17 16:24:06 -0500

    Setting up Hotel App Front-end

--- app.js
+++ app.js
@@ -6,19 +6,26 @@ var bodyParser = require('body-parser');
 
 var routes = require('./api/routes');
 
+//define the port to run on
 app.set('port', 3000);
 
+//add middleware to console log every request
 app.use(function(req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 
+//set status direcotry before define routes
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
+//enable parsing of posted forms
 app.use(bodyParser.urlencoded({ extended : false }));
 
+//add routing
 app.use('/api', routes);
 
+//listen for requests
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
     console.log('Port ' + port);
